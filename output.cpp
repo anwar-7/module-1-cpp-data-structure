0cf839e5e79943799a3766c297249901b302b3ca
anwar-7 <anwarjewel007@gmail.com>
2023-05-21 03:13:17 +0600

    practice input - output

---
+++ output.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // printf("Hello World"); -> for c
+    // std::cout<<"Hello World";
+    // std::cout<<"Hello\n";
+    // std::cout<<"World\n";
+    // std::cout<<"Hello "<<"World";
+    // std::cout<<"Hello"<<std::endl<<"World";
+    int a=100,b=50;
+    // printf("%d\n",a); //->for c
+    // std::cout<<a<<b;
+    // std::cout<<a<<" "<<b<<std::endl;
+    /* 
+    if we don't want use std again and again
+    than we can use a heder file "using namespace std;"
+     */
+    cout<<a<<" "<<b<<endl;
+    return 0;
+}
